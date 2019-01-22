0fb8b4961eb28e608780ca418431fa4824fd7c0a
17 <2540705109@qq.com>
2019-01-22 23:32:49 +0800

    start

---
+++ src/main/java/com/jiuhe/atcrowdfunding/AtcrowdfundingApplication.java
@@ -0,0 +1,14 @@
+package com.jiuhe.atcrowdfunding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtcrowdfundingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtcrowdfundingApplication.class, args);
+    }
+
+}
+
